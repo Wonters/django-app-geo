8e31db4b1825bc7e2ca1f4c3d935215460b56034
shift <shift.python.software@gmail.com>
2025-09-30 21:04:34 +0200

    clean

--- django_app_geo/apps.py
+++ django_app_geo/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GeoPointsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django-app-geo'
+    name = 'django_app_geo'
